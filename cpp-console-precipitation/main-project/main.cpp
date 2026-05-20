a6ed1e4cb33f611710801eab762ebbbf3cb7d026
2007andreychramtsov-dotcom <2007andreychramtsov@gmail.com>
2026-05-20 18:08:57 +0300

    Update main.cpp
    Добавлен вывод общей информации о задании

--- cpp-console-precipitation/main-project/main.cpp
+++ cpp-console-precipitation/main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #3. Precipitation\n";
+    cout << "Author: Andrey Khramtsov\n";
     return 0;
 }
