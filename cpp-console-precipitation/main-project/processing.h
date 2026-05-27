f1bb9874b8cf7fd18afe522d65598e41e53c83db
Andrey Chramtsov <2007andreychramtsov@gmail.com>
2026-05-27 08:12:05 +0300

    Добавлен вывод общего количества осадков за месяц

--- cpp-console-precipitation/main-project/processing.h
+++ cpp-console-precipitation/main-project/processing.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "precipitation.h"
+#include "processing.h"
 
 // Возвращает суммарное количество осадков за указанный месяц
 double process(const Precipitation* data, int count, int month);
